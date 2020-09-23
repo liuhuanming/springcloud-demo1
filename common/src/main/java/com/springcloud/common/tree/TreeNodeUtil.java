84b4b778d5f41619ee4fb899db844ea2ba4b5b73
liuhuanming <1334393253@qq.com>
2020-09-23 09:02:47 +0800

    增加平行树方法

--- common/src/main/java/com/springcloud/common/tree/TreeNodeUtil.java
+++ common/src/main/java/com/springcloud/common/tree/TreeNodeUtil.java
@@ -1,6 +1,7 @@
 package com.springcloud.common.tree;
 
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -68,5 +69,73 @@ public class TreeNodeUtil {
             }
         }
         return resultNode;
+    }
+
+    /**
+     * 获取当前节点及所有子节点的label标签集合
+     * @param node 集合
+     * @param <T> 类型
+     * @return List<String>
+     */
+    public static <T extends Node> List<String> getAllLabel(T node) {
+        List<String> labels = new ArrayList<>();
+        // 当前节点的label
+        labels.add(node.getLabel());
+        // 下属子节点的label
+        for (Node tempNode : node.getChildNodeList()) {
+            if (!CollectionUtils.isEmpty(tempNode.getChildNodeList())) { // 叶子节点，添加到结果中
+                labels.add(tempNode.getLabel());
+            } else { // 非叶子节点
+                List<String> childList = getAllLabel(node);
+                labels.addAll(childList);
+            }
+        }
+        return labels;
+    }
+
+    /**
+     * 构造多个平级树节点
+     * @param list 树数据
+     * @param <T> 类型
+     * @return 树
+     */
+    public static <T extends Node> List<T> buildMultiTree(List<T> list) {
+        List<T> treeNodes = new ArrayList<>();
+        List<T> rootNodes = getRootNodes(list);
+        T t = null;
+        for (int i = 0; i < rootNodes.size(); i++) {
+//            t = rootNodes.get(i);
+            t = buildChildTree(rootNodes.get(i), 0, list);
+            treeNodes.add(t);
+        }
+        return treeNodes;
+    }
+
+    /**
+     * 获取根节点，可以有N个根，parentId == null|| 等于0
+     * @param list 集合
+     * @param <T> 类型
+     * @return 返回集合中的根节点
+     */
+    public static <T extends Node> List<T> getRootNodes(List<T> list) {
+        List<T> rootNodes = new ArrayList<>();
+        for (T t : list) {
+            if (StringUtils.isEmpty(t.getParentId())||"0".equals(t.getParentId())) {
+                rootNodes.add(t);
+            }
+        }
+        return rootNodes;
+    }
+
+    public static <T extends Node> void clearNodeCount(T node) {
+        if (node!=null) {
+            node.setChildNodeList(null);
+            node.setChildNumCount(0);
+        }
+        if (CollectionUtils.isEmpty(node.getChildNodeList())) {
+            for (Node tempNode : node.getChildNodeList()) {
+                clearNodeCount(tempNode);
+            }
+        }
     }
 }
